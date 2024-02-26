78e630cc04ea333f573f921d268b73cff0c807df
santhoshreddybora <borasanthosh921@gmail.com>
2024-02-26 21:13:29 +0530

    EDA Done

---
+++ US_Visa/components/data_ingestion.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from US_Visa.logger import logging
+from US_Visa.utils import *
+from US_Visa.exception import CustomException
+
+
+class Data_Ingestion:
+    pass
